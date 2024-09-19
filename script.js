ba82c31d8143eaa326c785aa4ca14da7ae59fd85
jai karan S <155434150+Jaikarans2003@users.noreply.github.com>
2024-09-19 13:33:57 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // scripts.js
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz9EdYPvTFMbR-ymM-g-nRo-pshPoBFqA24iES640zqnSpfF96NrTvyYRgqnzF_p0Ljag/exec'; // Replace with your Google Apps Script URL
+const scriptURL = 'https://script.google.com/macros/s/AKfycbziwymUB8ikvJShHohQpMWzA6T863QzGllTrUo1lx_KpjnV5OiUM8RDTnfhk0BqOmuMKw/exec'; // Replace with your Google Apps Script URL
 const form = document.forms['submit-to-google-sheet'];
 
 
